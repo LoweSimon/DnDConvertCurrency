083466be317ed658a3a55b0beb24a310db2c126d
LoweSimon <simon.lowe90@hotmail.co.uk>
2022-09-21 13:02:47 +0100

    Added reset button. Started JS function.AddedParty

---
+++ function.js
@@ -0,0 +1,12 @@
+var c = document.getElementById("copper");
+var s = document.getElementById("silver");
+var e = document.getElementById("electrum");
+var g = document.getElementById("gold");
+var p = document.getElementById("platinum");
+
+function convert()  {
+    
+}
+
+
+document.getElementById("resultBody").innerHTML = "";
